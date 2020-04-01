8394ceb8fe069f2b24d399d83525c12c4910b3ba
Vaiva Ignataviciute <43579870+VaivaIgnataviciute@users.noreply.github.com>
2020-04-01 21:46:10 +0200

    2nd commit.
    Fetching data from API

--- App.js
+++ App.js
@@ -1,73 +1,161 @@
 import React from 'react';
-import {StyleSheet, Text, Platform, TextInput, KeyboardAvoidingView, ImageBackground, View} from 'react-native';
-import SearchInput from "./components/SearchInput"
-import getImageForWeather from "./utils/getImageForWeather";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
 
+import { fetchLocationId, fetchWeather } from './utils/api';
+import getImageForWeather from './utils/getImageForWeather';
 
-export default function App() {
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior = "height">
+import SearchInput from './components/SearchInput';
 
-      <ImageBackground 
-      source = {getImageForWeather("Light Cloud")}
-      style = {styles.imageContainer}
-      image style = {styles.image} >
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <View style = {styles.detailsContainer}>
+    this.state = {
+      loading: false,
+      error: false,
+      location: '',
+      temperature: 0,
+      weather: '',
+    };
+  }
 
-      <Text style = {[styles.largeText, styles.textStyle]}>
-        San Francisco</Text>
-        <Text style = {[styles.smallText, styles.textStyle]}>
-        Light Cloud</Text>
-        <Text style = {[styles.largeText, styles.textStyle]}>
-        24</Text>
+  componentDidMount() {
+    this.handleUpdateLocation('San Francisco');
+  }
 
-        <SearchInput
-        placeholder = "Search any city"
-      />
-      </View>
-      </ImageBackground>
-    </KeyboardAvoidingView>
-  );
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
+  render() {
+    const {
+      loading,
+      error,
+      location,
+      weather,
+      temperature,
+    } = this.state;
+
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="height"
+      >
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator
+              animating={loading}
+              color="white"
+              size="large"
+            />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different
+                    city.
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >
+                      {location}
+                    </Text>
+                    <Text
+                      style={[styles.smallText, styles.textStyle]}
+                    >
+                      {weather}
+                    </Text>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  
+    backgroundColor: '#34495E',
   },
-
-  textStyle: {
-    textAlign: "center",
-    fontFamily: Platform.OS === "ios" ? "AvenirNextRegular" : "Roboto",//checking in the font is supported on both ios and android
-  color: "white",
-  },
-  
-
-  largeText: {
-    fontSize: 44,
-  },
-
-  smallText: {
-    fontSize: 18,
-  },
-
   imageContainer: {
-flex:1,
+    flex: 1,
   },
   image: {
     flex: 1,
     width: null,
     height: null,
-    resizeMode: "cover",
+    resizeMode: 'cover',
   },
-
   detailsContainer: {
     flex: 1,
-    justifyContent: "center",
-    backgroundColor : "rgba(0,0,0,0.2)",
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
     paddingHorizontal: 20,
-  }
-  
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily:
+      Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
 });
--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,24 +1,62 @@
 import React from "react";
-import {StyleSheet, TextInput, View} from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import PropTypes from "prop-types";
+
+
 
 export default class SearchInput extends React.Component {
-    render () {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+        };
+    }
+    handleChangeText = (text) => {
+        this.setState({ text })
+    }
+    handleSubmitEditing = () => {
+        const { onSubmit } = this.props;
+        const { text } = this.state;
+
+        if (!text) return;
+
+        onSubmit(text);
+        this.setState({ text: "" });
+    }
+
+    render() {
+
+        const { placeholder } = this.props;
+        const { text } = this.state;
         return (
-            <View style = {styles.container}>
-               <TextInput style = {styles.TextInput}
-                autoCorrect = {false}
-                placeholder = {this.props.placeholder}
-                placeholderTextColor = "white"
-                underlineColorAndroid = "transparent"
-                clearButtonMode = "always" />
+            <View style={styles.container}>
+                <TextInput style={styles.TextInput}
+                    autoCorrect={false}
+                    value={text}
+                    placeholder={placeholder}
+                    placeholderTextColor="white"
+                    underlineColorAndroid="transparent" ß
+                    clearButtonMode="always"
+                    onChangeText={this.handleChangeText}
+                    onSubmitEditing={this.handleSubmitEditing} />
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create ({
+SearchInput.PropTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+};
+
+SearchInput.defaultProps = {
+    placeholder: ""
+}
+
+const styles = StyleSheet.create({
     container: {
-        height : 40,
+        height: 40,
         width: 300,
         marginTop: 20,
         backgroundColor: "#666",
@@ -30,7 +68,7 @@ const styles = StyleSheet.create ({
     },
 
     TextInput: {
-        flex:1,
+        flex: 1,
         color: "white"
     }
 
